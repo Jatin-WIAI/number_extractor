5d8c892c4eb91b431fb9b6266923e05ca267b773
Jatin-WIAI <jatin@wadhwaniai.org>
2023-04-13 12:57:41 +0000

    updated setup file for itn

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ import sys
 import os
 
 os.system("pip install -r requirements.txt")
+os.system("git clone https://github.com/Jatin-WIAI/indic-ITN.git")
+os.system("cd indic-ITN && bash install.sh")
+os.system("cd indic-ITN && python setup.py bdist_wheel")
+os.system("cd indic-ITN && pip install -e .")
+
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
